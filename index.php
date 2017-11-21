c0e02948bbf42176649b2c10417fff0eb22f5101
sremelis <sremelis@gmail.com>
2017-11-22 00:58:06 +0200

    f3-redirect
    / and /index.php redirects to searchPage.html, through f3 routing

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-	$f3 = require('M102/fatfree-master/lib/base.php');
-	//$db = require('M102/fatfree-master/lib/db/sql.php');
-	$db = new \DB\SQL('mysql:host=localhost;port=3306;dbname=M102','root','666999');
-	echo $db->driver().'<br />'; // mysql
+	$f3 = require('fatfree-master/lib/base.php');
+	//$db = require('fatfree-master/lib/db/sql.php');
+	//$db = new \DB\SQL('mysql:host=localhost;port=3306;dbname=M102','root','666999');
+	//echo $db->driver().'<br />'; // mysql
 	/*new Session();
 	$f3->set('SESSION.test',123);
 	echo $f3->get('SESSION.test');*/
 	new Session(NULL,'CSRF');
 	echo $f3->CSRF.'<br />'; // token here
-    //$f3 = require('M102/fatfree-master/lib/base.php');
-    //$f3 = require('M102/fatfree-master/lib/web/oauth2.php');
+    //$f3 = require('fatfree-master/lib/base.php');
+    //$f3 = require('fatfree-master/lib/web/oauth2.php');
 
 	class WebPage {
 		function display() {
@@ -18,7 +18,7 @@
 	}
 	$f3->route('GET /',
         function() {
-            echo 'Hello, Index!';
+            header('Location: searchPage.html');
         }
     );
 	$f3->route('GET /about', 'WebPage->display');
